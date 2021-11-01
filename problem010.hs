4cf94f61b4d95bd11f33a41a422cf3dc6c2a0b8b
Marco <marco.jakob1@mobi.ch>
2021-11-01 20:27:56 +0100

    problem 11 WIP

--- problem010.hs
+++ problem010.hs
@@ -1,9 +1,10 @@
 import PrimeHelper(primes)
 
+sievePrime :: Integral a => a -> [a]
 sievePrime n = [x | x <- [2..n], and [x `mod` y /= 0 | y <- [2..floor(sqrt(fromIntegral x))]]]
 
 primesBelowTwoMillion :: [Integer]
-primesBelowTwoMillion = [x | x <- (sievePrime 2000000)]
+primesBelowTwoMillion = [x | x <- sievePrime 2000000]
 
 main :: IO ()
 main = do
